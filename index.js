0467740d1bccfe006915177538a937519fb6d789
Boniface <bm715452@gmail.com>
2025-03-17 09:27:37 +0300

    arithmetic lab

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2; // multiply will be 62
 
-// Write your code here
+// random
+const random = Math.floor(Math.random() * 100) + 1; // Random integer greater than 0
+
+// mod
+const num3 = 24;
+const num4 = 5;
+const mod = num3 % num4; // mod will be 4
+
+// max
+const max = Math.max(10, 15, 20, 5); // max will be 20
+
+console.log(multiply); // should log 62
+console.log(random); // should log a random integer greater than 0
+console.log(mod); // should log 4
+console.log(max); // should log 20
